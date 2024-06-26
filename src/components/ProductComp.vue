a6baa91aff3407bf83d4032734ec9163bb38e664
matthew-dean-brown <mattrewbrown@gmail.com>
2024-06-26 15:08:37 +0200

    :sparkles:

--- src/components/ProductComp.vue
+++ src/components/ProductComp.vue
@@ -1,16 +1,54 @@
 <template>
-    <div>
-        <h1>
-            This is the product component✌️
-        </h1>
+    <div :style="{
+        backgroundImage: `url(${cover})`, 
+        backgroundSize:'cover', 
+        filter:'blur(4px)'
+     }" >
+        
     </div>
+    <div><h1>
+            This is the product component✌️
+            <!-- {{ bookTitle}} -->
+            <slot name="heading"></slot>
+            <slot name="author"></slot>
+            <slot name="pages"></slot>
+            <slot></slot>
+            
+            <!-- <br>
+            {{ bookTitle.title }}
+            <br>
+            {{ bookTitle.author }}
+            <br>
+            {{ bookTitle.year }}
+            <br>
+            {{ bookTitle.genre }}
+            <br>
+            {{ bookTitle.pages }}
+            <br>
+            {{ bookTitle.publisher }}
+            <br>
+            {{ bookTitle.isbn }} -->
+        </h1></div>
 </template>
 <script>
 
 export default {
-    
+    // props:['bookTitle']
+    props:['cover']
+    // props:{
+    //     bookTitle:Object
+    // }
 }
 </script>
 <style>
-    
+    h1{
+        color: #ffffff;
+        text-shadow: 3px 3px black;
+    }
+    div{
+        width: 300px;
+        height: 400px;
+        position: absolute;
+        border-radius: 10%;
+    }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,53 +1,55 @@
-<template lang="">
+<template>
   <div>
-    <!-- click event -->
-    <button v-on:click="decrement()">Decrement</button>
-    <h1>Counter -> <span>{{counter}}</span></h1>
-    <!-- click event -->
-    <button @click="increment()">Increment</button>
-    <!-- Attach javascript value to an attribute tag -->
-  </div>
-  <br>
-  
-  <!-- attribute binding -->
-  <img :src="imgUrl">
-  <div>
+    <div>
+      <!-- click event -->
+      <button v-on:click="decrement()">Decrement</button>
+      <h1>Counter -> <span>{{counter}}</span></h1>
+      <!-- click event -->
+      <button @click="increment()">Increment</button>
+      <!-- Attach javascript value to an attribute tag -->
+    </div>
+    <br>
     
-    <!-- Class styling binding -->
-    <h4 :class="{hello:check}">Conditional rendering</h4>
-    <!-- conditional rendering -->
-    <h1 v-if="check">The value is true</h1>
-    <h1 v-else>The value is false</h1>
-    <button @click="change()">Change state</button>
-    
-    <!-- conditional rendering -->
-    <h1 v-show="check">This contains the v-show</h1>
-    <ul>
-      <!-- list rendering -->
+    <!-- attribute binding -->
+    <img :src="imgUrl">
+    <div>
       
-      <!-- <li v-for="sport in sports" :key="sport">{{sport}}</li> -->
-      <li v-for="({message},index) in objects" :key="index">{{message}}</li>
-    </ul>
-    <!-- one way data binding -->
-    <h3>{{oneWayText}}</h3>
-    <!-- two way data binding -->
-    <h3>{{twoWayText}}</h3>
-    <input type="text" v-model="twoWayText">
+      <!-- Class styling binding -->
+      <h4 :class="{hello:check}">Conditional rendering</h4>
+      <!-- conditional rendering -->
+      <h1 v-if="check">The value is true</h1>
+      <h1 v-else>The value is false</h1>
+      <button @click="change()">Change state</button>
+      
+      <!-- conditional rendering -->
+      <h1 v-show="check">This contains the v-show</h1>
+      <ul>
+        <!-- list rendering -->
+        
+        <!-- <li v-for="sport in sports" :key="sport">{{sport}}</li> -->
+        <li v-for="({message},index) in objects" :key="index">{{message}}</li>
+      </ul>
+      <!-- one way data binding -->
+      <h3>{{oneWayText}}</h3>
+      <!-- two way data binding -->
+      <h3>{{twoWayText}}</h3>
+      <input type="text" v-model="twoWayText">
+    </div>
+    <!-- <info-comp first-name="hehe" last-name="    hehehehehe"/>
+    <info-comp first-name="hehe1"/>
+    <info-comp :object-data='singleObject'/> -->
+    <info-comp>
+      Hehe I am using slots
+    </info-comp>
+    <slot-comp>
+        <template #slotTitle>
+            <!-- Card Slot -->
+        </template>
+        <template #default>
+            <!-- <img :src="imgUrl"> -->
+        </template>
+    </slot-comp>
   </div>
-  <!-- <info-comp first-name="hehe" last-name="    hehehehehe"/>
-  <info-comp first-name="hehe1"/>
-  <info-comp :object-data='singleObject'/> -->
-  <info-comp>
-    Hehe I am using slots
-  </info-comp>
-  <slot-comp>
-      <template #slotTitle>
-          <!-- Card Slot -->
-      </template>
-      <template #default>
-          <!-- <img :src="imgUrl"> -->
-      </template>
-  </slot-comp>
 </template>
 <script>
 /* eslint-disable */
